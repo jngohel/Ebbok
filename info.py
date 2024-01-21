d74f3c2c7bd8f4d88ba4aca307cb83042a3638eb
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-01-21 14:27:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,23 +1,23 @@
 import os
 
-  API_ID = int(os.environ.get("API_ID", "12197477"))
-  API_HASH = os.environ.get("API_HASH", "ac077d8acc256685720b14eba02d8d11")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6144777379:AAHYgccuRRZRn-TT3_zBld4yQREDidoVke8")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "aks_file_store_bot")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001768689210"))
-  SHORTENER_WEBSITE = os.environ.get('SHORTENER_WEBSITE', "mdiskshortner.link")
-  SHORTENER_API = os.environ.get('SHORTENER_API', "e7beb3c8f756dfa15d0bec495abc65f58c0dfa95")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1030335104"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ttttechnicalaks7:filestorebotclone@cluster0.ch7domc.mongodb.net/?retryWrites=true&w=majority")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001831955243")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001769642119"))
-  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
-  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+API_ID = int(os.environ.get("API_ID", "12197477"))
+API_HASH = os.environ.get("API_HASH", "ac077d8acc256685720b14eba02d8d11")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6144777379:AAHYgccuRRZRn-TT3_zBld4yQREDidoVke8")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "aks_file_store_bot")
+DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001768689210"))
+SHORTENER_WEBSITE = os.environ.get('SHORTENER_WEBSITE', "mdiskshortner.link")
+SHORTENER_API = os.environ.get('SHORTENER_API', "e7beb3c8f756dfa15d0bec495abc65f58c0dfa95")
+BOT_OWNER = int(os.environ.get("BOT_OWNER", "1030335104"))
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ttttechnicalaks7:filestorebotclone@cluster0.ch7domc.mongodb.net/?retryWrites=true&w=majority")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001831955243")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001769642119"))
+BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 
-  ABOUT_BOT_TEXT = f"""This is a Permanent FileStore Bot. 
+ABOUT_BOT_TEXT = f"""This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
@@ -30,13 +30,13 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 │
 ╰──────[ 😎 ]───────────⍟"""
 
-  ABOUT_DEV_TEXT = f"""🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+ABOUT_DEV_TEXT = f"""🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
  
  I am Super noob Please Support My Hard Work.
 
 [Donate Me](https://t.me/KingVj01)"""
 
-  HOME_TEXT = """Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+HOME_TEXT = """Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
 How to Use Bot & it's Benefits??
 
