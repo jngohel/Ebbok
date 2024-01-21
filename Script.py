6cfa1704c2226cfbc70038c54c24c8acd9c5a315
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-01-21 19:48:37 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,24 @@
+class script(object):
+
+  START_TEXT = """<b>ᴛʜɪꜱ ɪꜱ ꜰɪʟᴇ,ᴠɪᴅᴇᴏꜱ,ɪᴍᴀɢᴇꜱ,ᴀᴜᴅɪᴏ ꜱᴀᴠᴇʀ ʙᴏᴛ. ꜱᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ, ᴠɪᴅᴇᴏꜱ, ɪᴍᴀɢᴇꜱ, ᴀᴜᴅɪᴏ ᴏʀ ᴀ ᴘᴏꜱᴛ ɪ ᴡɪʟʟ ɢɪᴠᴇ ʏᴏᴜ ᴀ ꜱʜᴀʀᴀʙʟᴇ ʟɪɴᴋ. ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ᴍᴏɴᴇʏ ʙʏ ꜱʜᴀʀɪɴɢ ᴛʜᴀᴛ ʟɪɴᴋ.
+
+🚫 ɴᴏᴛᴇ -  ᴀᴅᴜʟᴛ ʀᴇꜱᴛʀɪᴄᴛᴇᴅ, ɪꜰ ʏᴏᴜ ʙʀᴇᴀᴋɪɴɢ ʀᴜʟᴇꜱ ᴛʜᴇɴ ʏᴏᴜ ᴡɪʟʟ ᴘᴇʀᴍᴀɴᴇɴᴛ ʙᴀɴ</b>"""
+
+  ABOUT_BOT_TEXT = """This is a Permanent FileStore Bot. 
+Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+
+╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
+│
+├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+│
+├🔸 Language: [Python 3](https://www.python.org)
+│
+├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
+│
+╰──────[ 😎 ]───────────⍟"""
+
+    ABOUT_DEV_TEXT = """🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+ 
+ I am Super noob Please Support My Hard Work.
+
+[Donate Me](https://t.me/KingVj01)"""
