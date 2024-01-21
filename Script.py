86c14308deb28976cd4c58c1a35cf104f8093f90
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-01-21 20:27:59 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,21 +4,4 @@ class script(object):
 
 🚫 ɴᴏᴛᴇ -  ᴀᴅᴜʟᴛ ʀᴇꜱᴛʀɪᴄᴛᴇᴅ, ɪꜰ ʏᴏᴜ ʙʀᴇᴀᴋɪɴɢ ʀᴜʟᴇꜱ ᴛʜᴇɴ ʏᴏᴜ ᴡɪʟʟ ᴘᴇʀᴍᴀɴᴇɴᴛ ʙᴀɴ</b>"""
 
-  ABOUT_BOT_TEXT = """This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
-
-╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
-│
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
-│
-├🔸 Language: [Python 3](https://www.python.org)
-│
-├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
-│
-╰──────[ 😎 ]───────────⍟"""
-
-    ABOUT_DEV_TEXT = """🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
- 
- I am Super noob Please Support My Hard Work.
-
-[Donate Me](https://t.me/KingVj01)"""
+  FEATURES = """<b>adding soon...</b>"""
