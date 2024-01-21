82a635c663d1744714a899a0637e59ce104deeef
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-01-21 14:23:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,5 @@
 import os
 
-class Config(object):
   API_ID = int(os.environ.get("API_ID", "12197477"))
   API_HASH = os.environ.get("API_HASH", "ac077d8acc256685720b14eba02d8d11")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "6144777379:AAHYgccuRRZRn-TT3_zBld4yQREDidoVke8")
@@ -17,8 +16,8 @@ class Config(object):
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-  ABOUT_BOT_TEXT = f"""
-This is a Permanent FileStore Bot. 
+
+  ABOUT_BOT_TEXT = f"""This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
@@ -29,21 +28,18 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 │
 ├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
 │
-╰──────[ 😎 ]───────────⍟
-"""
-  ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+╰──────[ 😎 ]───────────⍟"""
+
+  ABOUT_DEV_TEXT = f"""🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
-"""
-  HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+[Donate Me](https://t.me/KingVj01)"""
+
+  HOME_TEXT = """Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
 How to Use Bot & it's Benefits??
 
 📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
 
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
-"""
+⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**."""
