c3ef1667000381c32736014e51d7a2cebc64f701
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-01 18:44:02 +0530

    Update add_user_to_db.py

--- handlers/add_user_to_db.py
+++ handlers/add_user_to_db.py
@@ -7,7 +7,4 @@ async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
         if LOG_CHANNEL is not None:
-            await bot.send_message(
-                int(LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!"
-            )
+            await bot.send_message(LOG_CHANNEL, f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!")
