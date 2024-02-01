9df576e1011c2b6e3b13c5abd3a5a96ff3aa9d0d
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-01 21:40:13 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -1,5 +1,5 @@
 import datetime
-from info import BOT_USERNAME, DATABASE_URL, LOG_CHANNEL, BOT_USERNAME 
+from info import BOT_USERNAME, DATABASE_URL
 from handlers.database import Database
 
 db = Database(DATABASE_URL, BOT_USERNAME)
@@ -8,4 +8,18 @@ async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id
     if not await db.is_user_exist(chat_id):
         await db.add_user(chat_id)
-        await bot.send_message(LOG_CHANNEL, f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!")
+        await bot.send_message(
+            Config.LOG_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+        )
+
+    ban_status = await db.get_ban_status(chat_id)
+    if ban_status["is_banned"]:
+        if (
+                datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
+        ).days > ban_status["ban_duration"]:
+            await db.remove_ban(chat_id)
+        else:
+            await cmd.reply_text("You Are Banned!..", quote=True)
+            return
+    await cmd.continue_propagation()
