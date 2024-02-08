3479e2d84d29890e6706ae2df510eb09bdcb005f
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-08 19:05:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,6 +15,7 @@ FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
 OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+BATCH_CHANNEL = int(os.environ.get("BATCH_CHANNEL", "-1002120917360"))
 
 #this vars for start message button
 SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP_LINK", "https://t.me/aks_bot_support")
