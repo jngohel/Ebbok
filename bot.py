5fb58d0754d32a0b77dfa2ff1e1bcc41c03846be
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2024-02-08 14:36:31 +0530

    Try to fix Batch File store

--- bot.py
+++ bot.py
@@ -6,15 +6,15 @@ from Script import script
 from pyrogram import Client, enums, filters
 from pyrogram.errors import UserNotParticipant, FloodWait, QueryIdInvalid
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message
-from info import DB_CHANNEL, AUTH_CHANNEL, BANNED_USERS, API_HASH, API_ID, BOT_USERNAME, BOT_TOKEN, LOG_CHANNEL, OTHER_USERS_CAN_SAVE_FILE, BOT_OWNER, BANNED_CHAT_IDS, SUPPORT_GROUP_LINK, UPDATES_CHANNEL_LINK, SHORTENER_WEBSITE, SHORTENER_API
+from info import DB_CHANNEL, AUTH_CHANNEL, BANNED_USERS, API_HASH, API_ID, BOT_USERNAME, BOT_TOKEN, LOG_CHANNEL, OTHER_USERS_CAN_SAVE_FILE, BOT_OWNER, BANNED_CHAT_IDS, SUPPORT_GROUP_LINK, UPDATES_CHANNEL_LINK, SHORTENER_WEBSITE, SHORTENER_API, BATCH_CHANNEL
 from handlers.database import db
 from handlers.add_user_to_db import add_user_to_database
-from handlers.send_file import send_media_and_reply
+from handlers.send_file import send_media_and_reply, reply_forward, delete_after_delay
 from handlers.helpers import b64_to_str, str_to_b64
 from handlers.check_user_status import handle_user_status
 from handlers.force_sub_handler import handle_force_sub, get_invite_link
 from handlers.broadcast_handlers import main_broadcast_handler
-from handlers.save_media import save_media_in_channel, save_batch_media_in_channel
+from handlers.save_media import save_media_in_channel, save_batch_media_in_channel, save_batch_onChannel
 
 MediaList = {}
 
@@ -151,7 +151,9 @@ async def start(bot: Client, cmd: Message):
             else:
                 message_ids.append(int(GetMessage.id))
             for i in range(len(message_ids)):
-                await send_media_and_reply(bot, user_id=cmd.from_user.id, file_id=int(message_ids[i]))
+                await send_media_and_reply(bot, user_id=cmd.from_user.id, file_id=int(message_ids[i]), uniqStr=(usr_cmd.split("_")[-1]))
+            await reply_forward(bot, cmd.from_user.id)
+            await delete_after_delay(uniqStr=(usr_cmd.split("_")[-1]), delay=1800)
         except Exception as e:
             print(e)
 
@@ -173,8 +175,6 @@ async def main(bot: Client, message: Message):
             return
 
         btn = [[
-            InlineKeyboardButton("ʙᴀᴛᴄʜ ʟɪɴᴋ", callback_data="genratebatchlink")
-        ],[
             InlineKeyboardButton("ꜱʜᴀʀᴇᴀʙʟᴇ ʟɪɴᴋ", callback_data="sharable_mode")
         ]]
         reply_markup=InlineKeyboardMarkup(btn)
@@ -355,6 +355,42 @@ async def _banned_users(_, m: Message):
         os.remove('banned-users.txt')
         return
     await m.reply_text(reply_text, True)
+
+@Bot.on_message(filters.command("batch") & filters.private)
+async def addBatch(bot: Client, message: Message):
+    cmd_txt = message.text
+    try:
+        link1 = int(((cmd_txt.split(" ", 3)[1]).split("t.me/c/", 2)[1]).split('/', 2)[1])
+        link2 = int(((cmd_txt.split(" ", 3)[2]).split("t.me/c/", 2)[1]).split('/', 2)[1])
+        linksList = [link1, link2]
+    except IndexError:
+        return await message.reply_text(text="Use proper format when using the command !\n\nFor example:\n```/batch firstmsgLink lastmsgLink```", quote=True)
+    else:
+        temp_msg1 = await bot.get_messages(chat_id=BATCH_CHANNEL, message_ids=link1)
+        if temp_msg1.document and temp_msg1.document.thumbs[0]: #check if the file is document and if it has thumbnail or not
+            thumb = temp_msg1.document.thumbs[0] #fetch thumb
+        elif temp_msg1.video and temp_msg1.video.thumbs[0]: #check if the file is video and if it has thumbnail or not
+            thumb = temp_msg1.video.thumbs[0] #fetch thumb
+        elif temp_msg1.audio and temp_msg1.audio.thumbs[0]: #check if the file is audio and if it has thumbnail or not
+            thumb = temp_msg1.audio.thumbs[0] #fetch thumb
+        else:
+            thumb = None #if file_type is not in ['document', 'video', 'audio']: assign None to thumb var
+        if thumb is None:
+            reply_msg = await message.reply_photo(
+                photo="https://icon-library.com/images/png-file-icon/png-file-icon-6.jpg",
+                caption="Please Wait...", 
+                quote=True
+            )
+        else:
+            thumb_jpg = await bot.download_media(thumb) #download thumb to current working dir
+            reply_msg = await message.reply_photo(
+                photo=thumb_jpg,
+                caption="Please Wait...",
+                quote=True,
+                parse_mode=enums.ParseMode.HTML
+            )
+            os.remove(thumb_jpg) #remove thumb from current working dir
+        await save_batch_onChannel(bot, message, reply_msg, linksList)
 
 @Bot.on_message(filters.private & filters.command("clear_batch"))
 async def clear_user_batch(bot: Client, m: Message):
--- handlers/save_media.py
+++ handlers/save_media.py
@@ -1,5 +1,5 @@
 import asyncio
-from info import DB_CHANNEL, LOG_CHANNEL, BOT_USERNAME
+from info import DB_CHANNEL, LOG_CHANNEL, BOT_USERNAME, BATCH_CHANNEL
 from pyrogram import Client, enums
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
@@ -181,6 +181,87 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 [
                     [
                         InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")
+                    ]
+                ]
+            )
+        )
+
+async def save_batch_onChannel(bot: Client, message: Message, edit_txt: Message, linksList: list):
+    try:
+        await edit_txt.edit_caption(
+            caption="<b>ᴘʀᴏᴄᴇꜱꜱɪɴɢ...</b>",
+            parse_mode=enums.ParseMode.HTML
+        )
+        fileCount = int(linksList[1]) - int(linksList[0])
+        i = 1
+        while (i<fileCount):
+            linksList += [(int(linksList[0])+i)]
+            i+=1
+        linksList.sort()
+        msg_ids = ""
+        for msg in (await bot.get_messages(chat_id=int(BATCH_CHANNEL), message_ids=linksList)):
+            if msg is None:
+                continue
+            to_DB = await msg.forward(DB_CHANNEL)
+            if to_DB is not None:
+                msg_ids += f"{to_DB.id} "
+            else:
+                continue
+        msg = await bot.send_message(
+            chat_id=DB_CHANNEL,
+            text=msg_ids,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Delete Batch", callback_data="close_data")
+                    ]
+                ]
+            )
+        )
+        user_id = message.from_user.id
+        user = await db.get_user(user_id)
+        share_link = f"https://telegram.me/{BOT_USERNAME}?start=VJBotz_{str_to_b64(str(msg.id))}"
+        short_link = await db.get_shortlink(user, share_link)
+        await edit_txt.edit_caption(
+            caption=f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: <code>{short_link}</code> \n\nJust Click the link to get your files!",
+            parse_mode=enums.ParseMode.MARKDOWN,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Original Link", url=share_link),
+                        InlineKeyboardButton("Short Link", url=short_link)
+                    ]
+                ]
+            )
+        )
+        await bot.send_message(
+            chat_id=int(LOG_CHANNEL),
+            text=f"#BATCH_SAVE:\n\n[{edit_txt.reply_to_message.from_user.first_name}](tg://user?id={edit_txt.reply_to_message.from_user.id}) Got Batch Link!",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Original Link", url=share_link),
+                        InlineKeyboardButton("Short Link", url=short_link)
+                    ]
+                ]
+            )
+        )
+    except Exception as err:
+        print(err)
+        await edit_txt.edit_caption(
+            caption=f"Something Went Wrong!\n\n**Error:** `{err}`",
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+        await bot.send_message(
+            chat_id=int(LOG_CHANNEL),
+            text=f"#ERROR_TRACEBACK:\nGot Error from `{str(edit_txt.chat.id)}` !!\n\n**Traceback:** `{err}`",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(edit_txt.chat.id)}")
                     ]
                 ]
             )
