23909abda8c50b85716d6793d811ba0c5809abbd
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-09 19:56:31 +0530

    Update save_media.py

--- AKS/save_media.py
+++ AKS/save_media.py
@@ -1,6 +1,6 @@
 import asyncio
 import traceback
-from info import DB_CHANNEL, LOG_CHANNEL, BOT_USERNAME, BATCH_CHANNEL, URL
+from info import DB_CHANNEL, LOG_CHANNEL, BOT_USERNAME, BATCH_CHANNEL, BIN_CHANNEL, URL
 from pyrogram import Client, enums
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
@@ -39,7 +39,9 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         file_er_id = str(forwarded_msg.id)
         user_id = message.from_user.id
         user = await db.get_user(user_id)
-        stream = f"https://{URL}/watch/{forwarded_msg.id}?hash={get_hash(forwarded_msg)}"
+        aks = await message.forward(BIN_CHANNEL)
+        aks_file = await bot.get_messages(BIN_CHANNEL, aks.id)
+        stream = f"https://{URL}/watch/{aks_file.id}?hash={get_hash(aks_file)}"
         link = f"https://telegram.me/{BOT_USERNAME}?start=Aks_{str_to_b64(file_er_id)}"
         short_link = await db.get_shortlink(user, stream)
         share_link = f"https://telegram.me/share/url?url={short_link}"
