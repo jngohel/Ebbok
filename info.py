f7d3fec17f4cd3665acf40cd4f5f0c80e01eb45f
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-10 15:44:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,9 +15,9 @@ FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
 OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-BATCH_CHANNEL = int(os.environ.get("BATCH_CHANNEL", "-1002120917360"))
 DELETE_TIME = int(os.environ.get('DELETE_TIME', 1200))
 
+#for stream
 BIN_CHANNEL = int(os.environ.get('BIN_CHANNEL', '-1001696019751'))
 URL = os.environ.get('URL', 'aks-file-to-link-525cd78edc50.herokuapp.com')
 
