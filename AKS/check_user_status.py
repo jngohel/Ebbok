c99ac89a8faa33222b6f4e6d7aedb1be72014870
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-10 14:54:17 +0530

    Update check_user_status.py

--- AKS/check_user_status.py
+++ AKS/check_user_status.py
@@ -9,6 +9,6 @@ async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id
     if not await db.is_user_exist(chat_id):
         await db.add_user(chat_id)
-        await bot.send_message(LOG_CHANNEL, text=script.NEW_USERS_TEXT.format(cmd.from_user.mention, cmd.from_user.id, {BOT_USERNAME}))
+        await bot.send_message(LOG_CHANNEL, text=script.NEW_USER_TEXT.format(cmd.from_user.mention, cmd.from_user.id, {BOT_USERNAME}))
     await cmd.continue_propagation()
             
