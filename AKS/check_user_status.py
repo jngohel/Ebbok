4465a8173443350db63b02c0a63dd6e9d66ca767
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-10 14:47:34 +0530

    Update check_user_status.py

--- AKS/check_user_status.py
+++ AKS/check_user_status.py
@@ -1,4 +1,5 @@
 import datetime
+from Script import script
 from info import BOT_USERNAME, DATABASE_URL, LOG_CHANNEL
 from AKS.database import Database
 
@@ -8,5 +9,6 @@ async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id
     if not await db.is_user_exist(chat_id):
         await db.add_user(chat_id)
-        await bot.send_message(LOG_CHANNEL, f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!")
+        await bot.send_message(LOG_CHANNEL, text=script.NEW_USERS_TEXT.format(cmd.from_user.mention, cmd.from_user.id, {BOT_USERNAME}))
     await cmd.continue_propagation()
+            
