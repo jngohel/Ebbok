02726da14fd6eb847674097e0820217f7d298b76
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-10 17:54:51 +0530

    Update send_file.py

--- AKS/send_file.py
+++ AKS/send_file.py
@@ -5,7 +5,7 @@ import string
 import random
 from info import DB_CHANNEL, FORWARD_AS_COPY, BOT_USERNAME, DELETE_TIME
 from pyrogram import Client
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
 from pyrogram.errors import FloodWait
 from AKS.helpers import str_to_b64, get_readable_time, calc, get_size
 from AKS.database import db
