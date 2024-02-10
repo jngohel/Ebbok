55e0326af04888ef3f09260a46eef6c7316ab510
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-10 14:44:51 +0530

    Update add_user_to_db.py

--- AKS/add_user_to_db.py
+++ AKS/add_user_to_db.py
@@ -1,10 +1,11 @@
 from info import LOG_CHANNEL, BOT_USERNAME
 from AKS.database import db
 from pyrogram import Client
+from Script import script
 from pyrogram.types import Message
 
 async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
         if LOG_CHANNEL is not None:
-            await bot.send_message(LOG_CHANNEL, f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!")
+            await bot.send_message(LOG_CHANNEL, script.NEW_USER_TEXT.format(cmd.from_user.mention, cmd.from_user.id, @{BOT_USERNAME}))
