34baaf210082bfdcb617439b938c7201250e151b
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-02-10 14:48:07 +0530

    Update add_user_to_db.py

--- AKS/add_user_to_db.py
+++ AKS/add_user_to_db.py
@@ -8,4 +8,4 @@ async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
         if LOG_CHANNEL is not None:
-            await bot.send_message(LOG_CHANNEL, script.NEW_USER_TEXT.format(cmd.from_user.mention, cmd.from_user.id, @{BOT_USERNAME}))
+            await bot.send_message(LOG_CHANNEL, script.NEW_USER_TEXT.format(cmd.from_user.mention, cmd.from_user.id, {BOT_USERNAME}))
